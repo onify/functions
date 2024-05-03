c3049b5c8182ce3405db9905f53bfb61bcffbdd7
Robert Lundsten <robert@onify.co>
2024-05-03 13:49:54 +0200

    fix: port and some logs

--- src/lib/setup-app.js
+++ src/lib/setup-app.js
@@ -7,7 +7,7 @@ import { logger, httpLogger } from './logger.js';
 
 dotenv.config();
 
-const port = process.env.ONIFY_API_INTERNAL_PORT || 8585;
+const port = process.env.PORT || 8585;
 const resourceURL = `${process.env.ONIFY_API_URL}/admin/resources/file?stream=false&path=`;
 const listResourcesURL = `${process.env.ONIFY_API_URL}/admin/resources?tree=true`;
 const resourcesHistoryURL = `${process.env.ONIFY_API_URL}/admin/resources/history`;
@@ -25,17 +25,22 @@ async function setupApp() {
   app.use(httpLogger);
 
   // Increase the limit for JSON payloads
-  app.use(express.json(({ limit: "50mb", extended: true, parameterLimit: 50000 })));
+  app.use(
+    express.json({ limit: '50mb', extended: true, parameterLimit: 50000 })
+  );
 
   // Increase the limit for plain text payloads
-  app.use(express.text(({ limit: "50mb", extended: true, parameterLimit: 50000 })));
+  app.use(
+    express.text({ limit: '50mb', extended: true, parameterLimit: 50000 })
+  );
 
   await nodeRun(app);
   // await pwshRun(app); TODO: To be implemented
 
-
   app.listen(port, () => {
-    logger.info(`Server is running on port ${port}`);
+    logger.info(
+      `Server is running on port ${port} in ${process.env.NODE_ENV} mode.`
+    );
   });
   return app;
 }
